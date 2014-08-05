2ab04612d9d22715fe3c97f7676c1c342f392934
OYYB <asdew@oy2b.local>
2014-08-05 23:41:46 +0900

    タブを増やした

---
+++ Grouper_app/app/src/main/java/org/sysken/grouper/Setting.java
@@ -0,0 +1,19 @@
+package org.sysken.grouper;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Setting extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedinstanceState){
+        //第三引数のbooleanは"container"にreturnするViewを追加するかどうか
+        //trueにすると最終的なlayoutに再度、同じView groupが表示されてしまうのでfalseでOKらしい
+        return inflater.inflate(R.layout.setting,container,false);
+    }
+}
