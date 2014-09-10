4d639d4303db82976f4930460339bb87bdb733c7
kunisada <r.kunisada661@gmail.com>
2014-09-10 16:48:19 +0900

    Modify validation

--- Grouper-Server/app/models/group.rb
+++ Grouper-Server/app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ActiveRecord::Base
   belongs_to :user
-  has_many :members
-  has_many :talks
+  has_many :members, :dependent => :destroy
+  has_many :talks, :dependent => :destroy
+
+  validates_presence_of :name, message: "グループ名は必須です"
 end
