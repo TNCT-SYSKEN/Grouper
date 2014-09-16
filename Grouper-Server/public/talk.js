f8020729115e62b18abc8ef00cd609d1fd2fe965
raryosu <hagi.1007.kosen@gmail.com>
2014-09-16 17:08:35 +0900

    fix talk and group

--- Grouper-Server/public/talk.js
+++ Grouper-Server/public/talk.js
@@ -5,6 +5,7 @@ $(function() {
   group_id = $('#gid').text();
   channel = dispatcher.subscribe(group_id);
   return channel.bind("new_message", function(message) {
-    return $(".talk_tbody").append('<tr class="talk_body"><th class="user"><p>' + message.name + '</p></th><th class="content"><p>' + message.talk + '</p></th></tr>');
+    // ユーザ画像が帰ってくるようにしたい
+    return $(".talk_tbody").append('<tr class="talk_user_name"><th colspan="3">' + message.name + '</tr><tr class="talk_body"><th class="user"><p></p></th><th class="content"><p>' + message.talk + '</p></th></tr>');
   });
 });
